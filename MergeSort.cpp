79a1c8b840284f3724fcb82b5a8e2d7d242c487e
zzaain <sheriellaghzain@gmail.com>
2026-04-02 15:00:11 +0700

    menambahkan fungsi input dan validasi panjang array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -6,3 +6,19 @@ int arr[20], B[20];
 // n is array input size
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
