faff101488c965fda650c4dbfdb324d178cf4bc9
zzaain <sheriellaghzain@gmail.com>
2026-04-02 15:01:12 +0700

    menambahkan  input isi array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -22,3 +22,13 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+ cout << "\n-------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "-------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
